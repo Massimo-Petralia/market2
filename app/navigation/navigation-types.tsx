c68ff8ffbc2656ac8f7e227747aa3fa7de396c28
Massimo <massipetra@hotmail.it>
2024-06-17 08:49:32 +0200

    feat:(models): mavigation ParamList types defined

---
+++ app/navigation/navigation-types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+    Main: undefined;
+    "Product detail": {id: number | null}
+}
+
+export type TabParamList = {
+    Home: undefined;
+    User: undefined;
+    Add: {id: number | null};
+    Cart: undefined //parametro provvisorio
+}
+
+export type UserStackParamList = {
+    Signin: undefined;
+    Signup: undefined
+}
+
--- app/navigation/navigation.tsx
+++ app/navigation/navigation.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { MainTabNavigator } from "./tab-navigation";
+import { ProductPage } from "../features/product/product-page";
 
 const RootStack = createNativeStackNavigator()
 
@@ -7,7 +8,7 @@ export const RootNavigator = () => {
     return (
         <RootStack.Navigator>
             <RootStack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }}/>
-            
+            <RootStack.Screen name="Product detail" component={ProductPage}/>
         </RootStack.Navigator>
     )
 }
