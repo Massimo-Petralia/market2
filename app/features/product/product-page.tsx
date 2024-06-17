ad137ff2f47558faf8686f881da9a427bd148ba6
Massimo <massipetra@hotmail.it>
2024-06-17 12:12:02 +0200

    -test- navigate from ProductPage to ProductsListPage OK

--- app/features/product/product-page.tsx
+++ app/features/product/product-page.tsx
@@ -1,10 +1,19 @@
 import { View } from "react-native"
 import { ProductView } from "./product-view"
+import { Button } from "react-native-paper"
+import { useNavigation } from "@react-navigation/native"
+import { HomeScreenTabNavigationProp} from "../../navigation/navigation-types"
 
 
 export const ProductPage = ()=> {
+  const navigation = useNavigation<HomeScreenTabNavigationProp>()
     return (
         <View>
+          <Button
+          mode="contained"
+          rippleColor='green'
+          onPress={()=> navigation.navigate('Home')}
+          >go back after delete</Button>
           <ProductView/>
         </View>
     )
--- app/features/products-list/products-list-page.tsx
+++ app/features/products-list/products-list-page.tsx
@@ -1,12 +1,18 @@
-import { View } from "react-native"
-import { Text } from "react-native-paper"
-import { ProductsListView } from "./products-list-view"
+import React from 'react';
+import {View} from 'react-native';
+import {ProductsListView} from './products-list-view';
+import {useFocusEffect} from '@react-navigation/native';
 
-
-export const ProductsListPage = ()=> {
-    return (
-        <View>
-           <ProductsListView/>
-        </View>
-    )
-}
+export const ProductsListPage = () => {
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log('Home (products list page) Log from effect');//avvolgi l'elaborazione dei dati in una condizione
+      //return qui pulisci l'effetto mettendo la condizione a false
+    }, []),
+  );
+  return (
+    <View>
+      <ProductsListView />
+    </View>
+  );
+};
