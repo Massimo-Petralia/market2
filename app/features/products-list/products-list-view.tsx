dbb4567d8ce58b9dd02c1d42da7674cc0f7f34b3
Massimo <massipetra@hotmail.it>
2024-06-17 10:22:04 +0200

    -test- navigate to product detail and viceversa OK

--- app/features/products-list/products-list-view.tsx
+++ app/features/products-list/products-list-view.tsx
@@ -1,11 +1,19 @@
-import { View } from "react-native"
-import { Text } from "react-native-paper"
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+import {Button} from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { ProductDetailScreenNavigationProp } from '../../navigation/navigation-types';
 
 
-export const ProductsListView = ()=> {
-    return (
-        <View>
-            <Text>product list View work !</Text>
-        </View>
-    )
-}
+export const ProductsListView = () => {
+    const navigation = useNavigation<ProductDetailScreenNavigationProp>()
+  return (
+    <View>
+      <Text>product list View work !</Text>
+      <Button mode="contained"
+      rippleColor='green'
+      onPress={()=> navigation.navigate('Product detail', {id: 1})}
+      >go to product detail</Button>
+    </View>
+  );
+};
