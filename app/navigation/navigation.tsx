bd32f5f4fb35b358cc1c9412dd822794f02256c8
Massimo <massipetra@hotmail.it>
2024-06-18 22:23:26 +0200

    branch feature task changed from "user" to "theme"

--- app/navigation/navigation.tsx
+++ app/navigation/navigation.tsx
@@ -2,15 +2,20 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MainTabNavigator} from './tab-navigation';
 import {ProductPage} from '../features/product/product-page';
 import  { RootStackParamList } from './navigation-types';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer , DefaultTheme} from '@react-navigation/native';
 import Routes from './navigation-routes';
+
+import {  adaptNavigationTheme } from 'react-native-paper';
+const {DarkTheme} = adaptNavigationTheme({reactNavigationDark: DefaultTheme})
+
 
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   return (
-    <NavigationContainer>
+    
+    <NavigationContainer theme={DarkTheme}>
       <RootStack.Navigator initialRouteName={Routes.root.main}>
         <RootStack.Screen
           name={Routes.root.main}
@@ -20,5 +25,6 @@ export const RootNavigator = () => {
         <RootStack.Screen name={Routes.root.itemDetail} component={ProductPage} />
       </RootStack.Navigator>
     </NavigationContainer>
+   
   );
 };
--- app/navigation/tab-navigation.tsx
+++ app/navigation/tab-navigation.tsx
@@ -5,12 +5,38 @@ import {ProductPage} from '../features/product/product-page';
 import {CartPage} from '../features/cart/cart-page';
 import {TabParamList} from './navigation-types';
 import Routes from './navigation-routes';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useTheme } from 'react-native-paper';
 
 const Tab = createMaterialBottomTabNavigator<TabParamList>();
 
 export const MainTabNavigator = () => {
+    const theme = useTheme()
   return (
-    <Tab.Navigator initialRouteName={Routes.root.tab.home}>
+    <Tab.Navigator initialRouteName={Routes.root.tab.home}
+    screenOptions={({route})=>({
+        tabBarIcon : ({focused}) => {
+            const size = 26;
+            let iconName = '';
+            let color = '';
+            if(route.name === Routes.root.tab.home){
+                iconName = focused ? 'home-circle' : 'home-circle-outline'
+                color = focused ? theme.colors.primary : theme.colors.secondary
+            }else if (route.name === Routes.root.tab.user.index) {
+                iconName = focused ? 'account-circle' : 'account-circle-outline'
+                color = focused ? theme.colors.primary : theme.colors.secondary
+            }else if(route.name === Routes.root.tab.Add) {
+                iconName = focused ? 'storefront' : 'storefront-outline'
+                color = focused ? theme.colors.primary : theme.colors.secondary
+            }else if (route.name === Routes.root.tab.cart) {
+                iconName = focused ? 'cart': 'cart-outline'
+                color = focused ? theme.colors.primary : theme.colors.secondary
+            }
+
+            return <MaterialCommunityIcons name={iconName} size={size} color={color}/>
+        }
+    })}
+    >
       <Tab.Screen name={Routes.root.tab.home} component={ProductsListPage} />
       <Tab.Screen name={Routes.root.tab.user.index} component={UserPage} />
       <Tab.Screen name={Routes.root.tab.Add} component={ProductPage} />
